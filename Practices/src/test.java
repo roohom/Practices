ecb719fd39811b929861d795fa35bb2a90a62ccd
roohom <roohom@qq.com>
2020-07-11 17:50:43 +0800

    JavaTest

---
+++ Practices/src/test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args){
+        int age;
+        age = 19;
+        System.out.println("println的打印输出会自动换行，并且可以不接收参数");
+        System.out.print("print的打印输出不会自动换行，并且其必须接收一个参数，如果不接收参数，在执行的时候会报错！");
+    }
+}
