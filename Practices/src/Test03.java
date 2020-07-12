880ac1a4f30076559cc5cc11c461fd5604cab9f5
roohom <roohom@qq.com>
2020-07-12 20:26:29 +0800

    JavaTest

---
+++ Practices/src/Test03.java
@@ -0,0 +1,20 @@
+/**
+ * ClassName: Test03
+ * Author: Roohom
+ * File: Test03
+ * Date: 2020/7/12 19:20
+ * Software: IntelliJ IDEA
+ */
+
+public class Test03 {
+    public static void main(String[] args) {
+        System.out.println("不忘初心方得始终");
+        System.out.println(88);
+        System.out.println(-88);
+        System.out.println(88.888);
+        System.out.println('A');
+        System.out.println('8');
+        System.out.println(true);
+        System.out.println(false);
+    }
+}
--- Practices/src/test.java
+++ Practices/src/test.java
@@ -1,8 +1,31 @@
 public class test {
-    public static void main(String[] args){
-        int age;
-        age = 19;
-        System.out.println("println的打印输出会自动换行，并且可以不接收参数");
-        System.out.print("print的打印输出不会自动换行，并且其必须接收一个参数，如果不接收参数，在执行的时候会报错！");
+    public static void main(String[] args) {
+        //打印字符串
+        String s = "不忘初心方得始终";
+        System.out.println(s);
+        System.out.println("------------------------");
+
+        //打印整数常量
+        int i1 = 88, i2 = -88;
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println("------------------------");
+
+        //打印小数常量
+        float d = 88.888f;
+        System.out.println(d);
+        System.out.println("------------------------");
+
+        //打印字符常量
+        char ch1 = 'A', ch2 = '8';
+        System.out.println(ch1);
+        System.out.println(ch2);
+        System.out.println("------------------------");
+
+
+        //打印布尔常量
+        boolean flag1 = true, flag2 = false;
+        System.out.println(flag1);
+        System.out.println(flag2);
     }
 }
