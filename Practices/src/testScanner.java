62ab5a768e49e9101bf950c9f5dd9ac5b7bf7c70
rooho <roohom@qq.com>
2020-07-12 14:32:50 +0800

    JavaTest

--- Practices/src/testScanner.java
+++ Practices/src/testScanner.java
@@ -1,5 +1,3 @@
-import sun.awt.geom.AreaOp;
-
 /**
  * ClassName: testScanner
  * Author: Roohom
@@ -8,16 +6,23 @@ import sun.awt.geom.AreaOp;
  * Software: IntelliJ IDEA
  */
 
+
+
+
 import java.util.Scanner;
 
 public class testScanner {
     public static void main(String[] args) {
-        System.out.println("本案例用于尝试Scanner扫描接收用户的输入");
+        System.out.println("本案例用于尝试Scanner扫描接收用户的输入:");
 
         //创建Scanner类
         Scanner sc = new Scanner(System.in);
 
+        //创建scanner对象
         String s1 = sc.next();
-        System.out.println(s1);
+
+        //输出接收到参数
+        System.out.println("你输入的是"+s1);
+
     }
 }
