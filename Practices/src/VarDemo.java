e28be110dd4a609db8925887f211ec2403ab0efe
roohom <roohom@qq.com>
2020-07-12 17:59:29 +0800

    JavaTest

---
+++ Practices/src/VarDemo.java
@@ -0,0 +1,13 @@
+/**
+ * ClassName: VarDemo
+ * Author: Roohom
+ * File: VarDemo
+ * Date: 2020/7/12 15:45
+ * Software: IntelliJ IDEA
+ */
+public class VarDemo {
+    public static void main(String[] args) {
+        int count =67;
+        System.out.println(count);
+    }
+}
