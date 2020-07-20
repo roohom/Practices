bbc5bf42d08472e3cdff7763328de1c27633844b
roohom <roohom@qq.com>
2020-07-20 23:11:35 +0800

    HomeWork UPDATE

--- Practices/src/com/roohom/demo/homework/day05/hw07.java
+++ Practices/src/com/roohom/demo/homework/day05/hw07.java
@@ -1,26 +1,45 @@
 package com.roohom.demo.homework.day05;
 
-import org.omg.CORBA.MARSHAL;
-
 import java.util.Arrays;
 
 /**
  * ClassName: hw07
  * Author: Roohom
  * Function:
+ *    	1. 创建两个长度为10的数组，数组内元素为随机生成的、不重复的 1-100之间的整数，
+ *    	2. 定义一个方法，传入两个数组，函数中将两个数组不同的元素拼接成一个字符串，并打印该字符串
+ *         如果没有则输出"对不起两个数组的所有元素均相同"
  * Date: 2020/7/18 20:54
  * Software: IntelliJ IDEA
  */
 public class hw07 {
-    //判断两个数组是不是完全相等
-    public static boolean equals(int[] arr1, int[] arr2)
-    {
-        if (arr1.length!=arr2.length)
+    /**
+     * Function:判断随机数在数组中是否重复(已存在)
+     *
+     * @param arr 目标数组
+     * @param num 判断的整数
+     * @return 布尔值,true代表无重复，false代表有重复
+     */
+    public static boolean isRepatNum(int[] arr, int num) {
+        for (int i = 0; i < arr.length; i++) {
+            if (num == arr[i])
+                return false;
+        }
+        return true;
+    }
+
+    /**
+     * Function:判断两个数组是不是完全相等
+     *
+     * @param arr1 传入数组
+     * @param arr2 传入数组
+     * @return 布尔值结果
+     */
+    public static boolean equals(int[] arr1, int[] arr2) {
+        if (arr1.length != arr2.length)
             return false;
-        for (int i=0;i<10;i++)
-        {
-            if (arr1[i]!=arr2[i])
-            {
+        for (int i = 0; i < 10; i++) {
+            if (arr1[i] != arr2[i]) {
                 return false;
             }
         }
@@ -31,60 +50,59 @@ public class hw07 {
     //测试: 2 3 4 5 6 7 8 9 0 0
     //两个数组中不同的值:1 7 8 9 0 0
     //拼接后应该就是178900
-    public static String splice(int[] arr1, int[] arr2)
-    {
-        if (equals(arr1,arr2))
-            System.out.println("对不起两个数组所有元素均相同！");
-        int[] newArr = new int[20];
-        for (int i=0;i<arr1.length;i++)
-        {
-            for (int j=0;j<arr2.length;j++)
-            {
-                if (arr1[i]!=arr2[j] && j==arr2.length-1)
-                {
-                    newArr[i] = arr1[i];
-                }
-            }
-        }
-        for (int i=0;i<arr2.length;i++)
-        {
-            for (int j=0;j<arr1.length;j++)
-            {
-                if (arr2[i]!=arr1[j] && j==arr1.length-1)
-                {
-                    newArr[10+i] = arr2[i];
-                }
-            }
-        }
-        String str=null;
-        for (int k=0;k<20;k++)
-        {
-            //生成的是1~100的数，不可能有0，所以位置上是0的肯定是创建新数组时初始化默认的
-            if (newArr[k]!=0)
-            {
-                str += newArr[k];
-            }
-        }
 
+    /**
+     * function: 将两个数组中的元素进行拼接成字符串
+     *
+     * @param arr1 传入数组1
+     * @param arr2 传入数组2
+     * @return 字符串结果
+     */
+    public static String splice(int[] arr1, int[] arr2) {
+        if (equals(arr1, arr2))
+            System.out.println("对不起两个数组所有元素均相同！");
+        //int[] newArr = new int[arr1.length+arr2.length];
+        String str = "";
+        for (int i = 0; i < arr1.length; i++) {
+            boolean flag = isRepatNum(arr2,arr1[i]);
+            if (flag)
+                str += arr1[i]+",";
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            boolean flag = isRepatNum(arr2,arr1[i]);
+            if (flag)
+                str += arr2[i]+",";
+        }
         return str;
+    }
+
+    /**
+     * Function: 生成数字不重复的且在1~100之间的数组
+     * @param arr 传入的空数组
+     */
+    public static void randomArray(int[] arr)
+    {
+        int index = 0;
+        do {
+            int rdm = (int) (Math.random() * 100 + 1);
+            if (isRepatNum(arr, rdm)) {
+                arr[index] = rdm;
+                index++;
+            }
+        } while (index != arr.length);
     }
 
     public static void main(String[] args) {
         int[] arr1 = new int[10];
         int[] arr2 = new int[10];
-
-        for (int i=0;i<10;i++)
-        {
-            arr1[i] = (int)(Math.random()*100);
-        }
-        for (int j=0;j<10;j++)
-        {
-            arr2[j] = (int)(Math.random()*100);
-        }
+        randomArray(arr1);
+        randomArray(arr2);
+//        int[] arr1 = {53, 73, 35, 75, 36, 4, 40, 57, 16, 21};
+//        int[] arr2 = {11, 99, 56, 95, 49, 53, 76, 26, 4, 6};
         System.out.println(Arrays.toString(arr1));
         System.out.println(Arrays.toString(arr2));
 
-        System.out.println(splice(arr1,arr2));
+        System.out.println(splice(arr1, arr2));
 
     }
 }
--- Practices/src/com/roohom/demo/homework/day05/oddLeftEvenRight.java
+++ Practices/src/com/roohom/demo/homework/day05/oddLeftEvenRight.java
@@ -1,46 +1,66 @@
 package com.roohom.demo.homework.day05;
-
-import javax.swing.*;
-import java.util.Arrays;
 import java.util.Scanner;
 
 /**
  * ClassName: hw05
  * Author: Roohom
  * Function:
+ *  1.键盘录入10个整数存入数组中
+ * 	2.定义一个方法将奇数放在数组的左侧,偶数放在数组的右侧
+ * 	3.定义一个方法打印原数组和处理后的数组
+ * 	4.定义一个方法传入一个int类型数组，输出这个数组中只出现一次的数字及个数
  * Date: 2020/7/18 19:33
  * Software: IntelliJ IDEA
  */
 
-//未完成！
+public class oddLeftEvenRight {
+    /**
+     * Function:判断数组指定起止索引内元素是否都是偶数
+     *
+     * @param arr       目标数组
+     * @param fromIndex 起始索引
+     * @param endIndex  截止索引
+     * @return true代表都是偶数 false代表不是
+     */
+    public static boolean ifArrayEven(int[] arr, int fromIndex, int endIndex) {
+        for (int i = fromIndex; i <= endIndex; i++) {
+            if (arr[i] % 2 == 0) {
+                if (i == endIndex)
+                    return true;
+            }
+            else
+                return false;
+        }
+        return false;
+    }
 
-public class hw05 {
     //将奇数放在数组的左侧,偶数放在数组的右侧
     //从头向尾部扫描，如果扫描到奇数，不做操作，如果是偶数，把它赋值给temp保存，后面所有的数
     //向前移动一位，待移动完，把temp放置在末尾
+
+    /**
+     * Function: 把数组中的奇数都放在左边，偶数都放在右边
+     * @param arr 目标数组
+     */
     public static void oddLeftEvenRight(int[] arr) {
         int temp = 0;
         //int flag=0;
         // 测试:2 2 2 2 1 1 1 3 4 5
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i]%2==0) {
-                if (arr[i] % 2 == 0) {
-                    temp = arr[i];
-                    //idx = i;
-                    for (int j = i; j < arr.length - 1; j++) {
-                        arr[j] = arr[j + 1];
-                    }
-                    arr[arr.length - 1] = temp;
+            if (arr[i] % 2 == 0) {
+                temp = arr[i];
+                //idx = i;
+                //后面的元素向前移动一位
+                for (int j = i; j < arr.length - 1; j++) {
+                    arr[j] = arr[j + 1];
                 }
+                //把刚才的偶数置于末尾
+                arr[arr.length - 1] = temp;
+
                 //判断后面的位置是不是都是偶数，如果是就停止后面的操作，退出循环
-                for (int k=i+1;k<arr.length;k++)
-                {
-                    if (arr[k]%2==0)
-                    {
-                        if ( arr[k]%2==0 && k==arr.length-1)
-                            break;
-                    }
-                }
+                if (ifArrayEven(arr, i , arr.length - 1)){
+                    continue;}
+                // 如果该位置是偶数，为了防止后面的元素前移后该位置还是偶数，必须将i-1来使得下次判断偶数还从该位置开始
                 i--;
             }
         }
@@ -60,7 +80,7 @@ public class hw05 {
         //依次对每个位置上的数进行扫描，扫描的同时将此数与其他位置的数对比，如果相同，不做统计，如果不同，则计数
         // 双层循环
 
-        int[] tempArr =new int[arr.length];
+        int[] tempArr = new int[arr.length];
         for (int i = 0; i < arr.length; i++) {
             int count = 0;
             for (int j = 0; j < arr.length; j++) {
@@ -71,11 +91,9 @@ public class hw05 {
             tempArr[i] = count;
         }
         System.out.print("出现一次的数字有:");
-        for (int k=0;k<tempArr.length;k++)
-        {
-            if (tempArr[k]==1)
-            {
-                System.out.print(arr[k]+" ");
+        for (int k = 0; k < tempArr.length; k++) {
+            if (tempArr[k] == 1) {
+                System.out.print(arr[k] + " ");
             }
         }
     }
@@ -85,18 +103,29 @@ public class hw05 {
         //键盘录入10个整数存入数组
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[10];
+        System.out.println("请输入十个数，我将把奇数放左边，偶数放右边:");
+
+        //开始输入
         for (int i = 0; i < 10; i++) {
             arr[i] = sc.nextInt();
         }
+
         //打印原数组
         System.out.print("原   数  组:");
         printArray(arr);
         oddLeftEvenRight(arr);
         System.out.println();
+
         //打印处理后的数组
         System.out.print("处理后的数组:");
         printArray(arr);
         System.out.println();
+
+        //统计出现一次的数字并打印出来
         NumOnceAndCount(arr);
+        //测试
+//        int[] arr = {2,2,2,2,1,2};
+//        System.out.println(ifArrayEven(arr,0,5));
+
     }
 }
