db4e828ba51764874abfce4aa35c452c8643794a
roohom <roohom@qq.com>
2020-07-26 22:35:13 +0800

    java Collection

--- Practices/src/com/roohom/demo/homework/day10/IDcardToBirth/getBirthFromIDcard.java
+++ Practices/src/com/roohom/demo/homework/day10/IDcardToBirth/getBirthFromIDcard.java
@@ -1,8 +1,6 @@
 package com.roohom.demo.homework.day10.IDcardToBirth;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Scanner;
 
 /**
@@ -13,9 +11,9 @@ import java.util.Scanner;
  * Software: IntelliJ IDEA
  */
 public class getBirthFromIDcard {
-    public static void main(String[] args) throws ParseException {
-        System.out.println("请输入你的身份证号");
-        //341125199709091273
+    public static void main(String[] args){
+        System.out.println("请输入你的身份证号:");
+        //身份证号
         Scanner sc = new Scanner(System.in);
         String id_number = sc.next();
         StringBuilder sb = new StringBuilder(id_number);
@@ -24,9 +22,5 @@ public class getBirthFromIDcard {
         int str_birth3 =Integer.parseInt(sb.substring(12,14));
         String str_birth = str_birth1+"年"+ str_birth2+"月"+ str_birth3+"日";
         System.out.println(str_birth);
-
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日");
-        Date strDate = sdf.parse(str_birth);
-        System.out.println(sdf.format(strDate));
     }
 }
